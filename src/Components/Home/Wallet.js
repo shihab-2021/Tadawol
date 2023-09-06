a06f3058070b5b75406619ae0830161aa415d849
MdBayazid-star <freelencerbayazid@gmail.com>
2023-09-06 21:43:32 +0600

    Home

--- src/Components/Home/Wallet.js
+++ src/Components/Home/Wallet.js
@@ -1,78 +1,88 @@
-import React, { useState } from "react";
-import DialogLayout from "../Shared/DialogLayout";
+import React from "react";
 
 const Wallet = () => {
-  const [openMoneyOverview, setOpenMoneyOverview] = useState(true);
   return (
-    <div>
-      <DialogLayout
-        iconColor={"white"}
-        open={openMoneyOverview}
-        setOpen={setOpenMoneyOverview}
-        bgColor={"#005A58"}
-        width={800}
-        borderRadius={20}
-      >
-        <div>
-          <h1 className=" text-[#fff] font-[600] text-[17px] text-center my-[15px] ">
-            Money Overview
-          </h1>
-          <div className="grid grid-cols-2 gap-[12px]">
-            <div className="flex flex-col items-center justify-center gap-[10px] px-[20px] py-[40px] rounded-[20px] border-[1px] border-[#ffffff33] ">
-              <h2 className=" text-[20px] font-[600] text-[#fff] flex items-center ">
-                Total Wallet Balance{" "}
-                <span className="ml-[12px] w-[11px] h-[20px] ">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="12"
-                    height="21"
-                    viewBox="0 0 12 21"
-                    fill="none"
-                  >
-                    <path
-                      opacity="0.5"
-                      d="M11.5 10.5C11.5 10.212 11.3988 9.947 11.1851 9.73963L2.26585 0.799539C2.07464 0.603687 1.8272 0.5 1.53476 0.5C0.949898 0.5 0.5 0.949309 0.5 1.54839C0.5 1.83641 0.623722 2.10138 0.80368 2.29724L8.99182 10.5L0.80368 18.7028C0.623722 18.8986 0.5 19.1521 0.5 19.4516C0.5 20.0507 0.949898 20.5 1.53476 20.5C1.8272 20.5 2.07464 20.3963 2.26585 20.1889L11.1851 11.2604C11.3988 11.0415 11.5 10.788 11.5 10.5Z"
-                      fill="white"
-                    />
-                  </svg>
-                </span>
-              </h2>
-              <h1 className=" text-white text-[44px] font-[400] ">
-                <span className=" opacity-[0.5] text-[32px] mr-1 ">$</span>
-                27,250
-                <span className=" opacity-[0.5] text-[32px] ">.68</span>
-              </h1>
-              <h2 className=" text-[#D0EF7B] text-[28px] ">+11.6%</h2>
-            </div>
-            <div className="flex flex-col items-center justify-center gap-[10px] px-[20px] py-[40px] rounded-[20px] border-[1px] border-[#ffffff33] ">
-              <h2 className=" text-[20px] font-[600] text-[#fff] flex items-center ">
-                Total Wallet Balance{" "}
-                <span className="ml-[12px] w-[11px] h-[20px] ">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="12"
-                    height="21"
-                    viewBox="0 0 12 21"
-                    fill="none"
-                  >
-                    <path
-                      opacity="0.5"
-                      d="M11.5 10.5C11.5 10.212 11.3988 9.947 11.1851 9.73963L2.26585 0.799539C2.07464 0.603687 1.8272 0.5 1.53476 0.5C0.949898 0.5 0.5 0.949309 0.5 1.54839C0.5 1.83641 0.623722 2.10138 0.80368 2.29724L8.99182 10.5L0.80368 18.7028C0.623722 18.8986 0.5 19.1521 0.5 19.4516C0.5 20.0507 0.949898 20.5 1.53476 20.5C1.8272 20.5 2.07464 20.3963 2.26585 20.1889L11.1851 11.2604C11.3988 11.0415 11.5 10.788 11.5 10.5Z"
-                      fill="white"
-                    />
-                  </svg>
-                </span>
-              </h2>
-              <h1 className=" text-white text-[44px] font-[400] ">
-                <span className=" opacity-[0.5] text-[32px] mr-1 ">$</span>
-                27,250
-                <span className=" opacity-[0.5] text-[32px] ">.68</span>
-              </h1>
-              <h2 className=" text-[#D0EF7B] text-[28px] ">+11.6%</h2>
-            </div>
+    <div className="m-1 md:m-0 relative">
+      <div className=" hidden md:block text-xl font-semibold mb-0 md:mb-4">
+        Wallet
+      </div>
+      <div className=" bg-[#005A58] md:w-[541px] md:h-[240px] rounded-[20px] md:p-10 text-white pt-[81px] pb-14 px-7">
+        <p className="text-[17px] ">Total Wallet Balance </p>
+        <div className="flex justify-between ">
+          <div className="flex items-center">
+            <span className="text-4xl font-light pr-1">$</span>
+            <span className=" text-4xl font-medium">27,250</span>
+            <sub className="text-[28px] font-light opacity-50 flex items-end mr-1">
+              .68
+            </sub>
+            <span>
+              <svg
+                width="11"
+                height="21"
+                viewBox="0 0 11 21"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  opacity="0.5"
+                  d="M11 10.5C11 10.212 10.8988 9.947 10.6851 9.73963L1.76585 0.799539C1.57464 0.603687 1.3272 0.5 1.03476 0.5C0.449898 0.5 0 0.949309 0 1.54839C0 1.83641 0.123722 2.10138 0.30368 2.29724L8.49182 10.5L0.30368 18.7028C0.123722 18.8986 0 19.1521 0 19.4516C0 20.0507 0.449898 20.5 1.03476 20.5C1.3272 20.5 1.57464 20.3963 1.76585 20.1889L10.6851 11.2604C10.8988 11.0415 11 10.788 11 10.5Z"
+                  fill="white"
+                />
+              </svg>
+            </span>
+          </div>
+          <div>
+            <a
+              href="/"
+              className="absolute md:relative -bottom-8 md:-bottom-0 md:border-0 border-4 md:left-0 left-40 rounded-full border-white"
+            >
+              <svg
+                width="56"
+                height="56"
+                viewBox="0 0 56 56"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  x="-3.05176e-05"
+                  width="56"
+                  height="56"
+                  rx="28"
+                  fill="#D9F867"
+                />
+                <path
+                  d="M37 28.1924L19 28.1924"
+                  stroke="#005A58"
+                  stroke-width="3"
+                  stroke-linecap="round"
+                />
+                <path
+                  d="M28.1924 37L28.1924 19"
+                  stroke="#005A58"
+                  stroke-width="3"
+                  stroke-linecap="round"
+                />
+              </svg>
+            </a>
           </div>
         </div>
-      </DialogLayout>
+        <div className="flex justify-between mt-5">
+          <div>
+            <p className="text-[13px]">Profit/Loss</p>
+            <p className=" text-xl  text-[#D0EF7B]">+11.6%</p>
+          </div>
+          <span className="border-r-2 border-white opacity-25"></span>
+          <div>
+            <p className="text-[13px]">Used Margin</p>
+            <p className=" text-2xl text-white ">$ 11,000.68</p>
+          </div>
+          <span className="border-r-2 border-white opacity-25"></span>
+          <div>
+            <p className="text-[13px]">Available Margin</p>
+            <p className=" text-xl text-white">$ 16,250</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/Home/WatchList.js
+++ src/Components/Home/WatchList.js
@@ -1,7 +1,50 @@
 import React from "react";
-
+import Apple from "./../../Assets/Home/Logo.jpg";
+import Google from "./../../Assets/Home/Logo (1).png";
+import ie from "./../../Assets/Home/Logo (2).png";
+import Right from "./../../Assets/Home/Logo (3).png";
 const WatchList = () => {
-  return <div>this is watch list</div>;
+  return (
+    <div className="">
+      <h2 className=" text-xl font-semibold mb-4">Watchlist</h2>
+      <div className="p-6 w-[312px] h-[240px] rounded-[20px] shadow">
+        <div className="flex">
+          <img className=" z-10" src={Apple} alt="" />
+          <img className="-ml-5 z-0" src={Google} alt="" />
+          <img className="-ml-5" src={ie} alt="" />
+          <img className="-ml-5" src={Right} alt="" />
+        </div>
+        <div>
+          <h5 className=" text-center text-[15px] font-normal mt-3">
+            Stay updated on the most recent advancements in the markets you've
+            included in your watchlist.
+          </h5>
+        </div>
+        <div>
+          <a
+            href="/"
+            className=" text-center text-[#8A8A8E] flex items-center justify-center mt-2"
+          >
+            Go to my Watchlist{" "}
+            <svg
+              className="ml-1 h-10"
+              xmlns="http://www.w3.org/2000/svg"
+              width="8"
+              height="12"
+              viewBox="0 0 8 12"
+              fill="none"
+            >
+              <path
+                d="M7.49774 6C7.49329 5.86192 7.46656 5.73497 7.41756 5.61915C7.36411 5.50334 7.28393 5.39198 7.17703 5.28508L2.03226 0.253898C1.863 0.0846325 1.65587 0 1.41088 0C1.24162 0 1.09017 0.0400891 0.956539 0.120267C0.818454 0.200445 0.709323 0.309577 0.629145 0.447661C0.544512 0.581292 0.502196 0.730512 0.502196 0.895323C0.502196 1.14477 0.595737 1.36303 0.78282 1.55011L5.35966 6L0.78282 10.4499C0.595737 10.6414 0.502196 10.8597 0.502196 11.1047C0.502196 11.2739 0.544512 11.4254 0.629145 11.559C0.709323 11.6927 0.818454 11.7996 0.956539 11.8797C1.09017 11.9599 1.24162 12 1.41088 12C1.65587 12 1.863 11.9154 2.03226 11.7461L7.17703 6.71492C7.28839 6.60802 7.37079 6.49666 7.42425 6.38085C7.47324 6.26058 7.49774 6.13363 7.49774 6Z"
+                fill="#3C3C43"
+                fill-opacity="0.6"
+              />
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WatchList;
