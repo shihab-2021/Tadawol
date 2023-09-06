590e880b9ec802bc96f22770236057c54742df16
Shajibul Alam Shihab <shihab77023@gmail.com>
2023-09-06 22:38:26 +0600

    update

--- src/Components/Shared/Layout.js
+++ src/Components/Shared/Layout.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 // import { Link, useLocation } from "react-router-dom";
 import Logo from "../../Assets/Shared/Logo.png";
+import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
 
 const Layout = ({ children }) => {
   const [toggleButton, setToggleButton] = useState(true);
@@ -91,46 +93,7 @@ const Layout = ({ children }) => {
                         {/* <img src={ArrowLeftIcon} alt="icon" /> */}X
                       </button>
                     </div>
-                    <ul className="space-y-2 px-[22px] py-2 text-white">
-                      <li>
-                        {/* <Link
-                          style={
-                            location.pathname === "/dashboard"
-                              ? {
-                                  background:
-                                    "linear-gradient(270deg, rgba(0, 0, 0, 0.45) 0%, rgba(0, 0, 0, 0.274309) 35.55%, rgba(0, 0, 0, 0) 100%), #6278FF",
-                                }
-                              : {}
-                          }
-                          to="/dashboard"
-                          className={`text-white font-normal rounded-[15px] flex items-center px-[20px] py-[13px]  group`}
-                        >
-                          {location.pathname === "/dashboard" ? (
-                            <img
-                              className=""
-                              // src={DashboardIconLight}
-                              alt="icon"
-                            />
-                          ) : (
-                            <img
-                              className=""
-                              // src={DashboardIconDark}
-                              alt="icon"
-                            />
-                          )}
-
-                          <span
-                          className={`${
-                            location.pathname === "/dashboard"
-                              ? "text-white"
-                              : "text-[#8F8F8F]"
-                          } ml-3 text-[18px] font-[500]`}
-                          >
-                            Dashboard
-                          </span>
-                        </Link> */}
-                      </li>
-                    </ul>
+                    <Sidebar />
                   </div>
                 </div>
               </div>
@@ -143,9 +106,7 @@ const Layout = ({ children }) => {
               id="main-content"
               className="h-full w-full relative lg:ml-[250px]"
             >
-              <nav className="h-[70px] border-b-[1px]">
-                <div>this is navbar</div>
-              </nav>
+              <Navbar />
               <main className="min-h-[100vh]">
                 <div className="">{children}</div>
               </main>
