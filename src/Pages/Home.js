c5c96e393624fa40a5da220439b1e4d0d36113e9
Shajibul Alam Shihab <shihab77023@gmail.com>
2023-09-06 22:31:09 +0600

    update

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,11 +3,13 @@ import Layout from "../Components/Shared/Layout";
 import Wallet from "../Components/Home/Wallet";
 import WatchList from "../Components/Home/WatchList";
 import TopTraders from "../Components/Home/TopTraders";
+import Trades from "../Components/Home/Trades";
 
 const Home = () => {
   return (
     <div>
       <Layout>
+        <Trades />
         <Wallet />
         <WatchList />
         <TopTraders />
