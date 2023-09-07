4a4129f335abdeacd5932bea72f1dc4da7945b30
sohan <tanvirsohaniiuc@gmail.com>
2023-09-07 23:00:26 +0600

    update

--- src/Components/Shared/Navbar.js
+++ src/Components/Shared/Navbar.js
@@ -1,9 +1,50 @@
 import React from "react";
+import SearchIcon from '@mui/icons-material/Search';
+import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import profile from '../../Assets/Navbar/profile.png';
+import Label from '../../Assets/Navbar/Label.png';
 
 const Navbar = () => {
   return (
     <nav className="h-[70px] border-b-[1px]">
-      <div>this is navbar</div>
+      <div className="flex pt-[10px] justify-between items-center">
+        <div className="w-[50%] h-[38px] flex items-center px-4 text-[17px] font-normal ms-[48px]"
+          style={{
+            borderRadius: "100px",
+            background: "var(--system-background-light-secondary, #F2F2F7)"
+          }}
+        >
+          <SearchIcon />
+          <input
+            style={{
+              background: "var(--system-background-light-secondary, #F2F2F7)"
+            }}
+            className="w-[100%] mx-2" type="text" placeholder="Search for stock, crypto, NTF ..." />
+          <KeyboardVoiceIcon />
+        </div>
+        <div className="w-[50%] flex items-center justify-end me-[300px]">
+          <div>
+            <NotificationsIcon  />
+          </div>
+          <div className="ms-[29px]">
+            <h1 className="text-[17px] font-semibold">Mohammed Al-Haj</h1>
+            <p className="flex text-[15px] font-normal"
+            style={{
+              color: "var(--label-color-light-secondary, rgba(60, 60, 67, 0.60))"
+            }}
+            > <img src={Label} alt="icon"/>
+             <span className="h-[5px] w-[5px] bg-[#3C3C4399] rounded-full mt-2 mx-1"></span>
+             Expert Trader </p>
+          </div>
+          <div className="bg-[#EAF3E6] rounded-[100px] h-[44px] w-[44px] flex justify-center items-center ms-3">
+            <img src={profile} alt="profile"/>
+
+          </div>
+
+        </div>
+
+      </div>
     </nav>
   );
 };
--- src/Components/Shared/Sidebar.js
+++ src/Components/Shared/Sidebar.js
@@ -1,47 +1,108 @@
 import React from "react";
+import home from "../../Assets/Navbar/Home.png";
+import WatchingList from "../../Assets/Navbar/WatchingList.png";
+import Wallet from "../../Assets/Navbar/Wallet.png";
+import Search from "../../Assets/Navbar/Search.png";
+import MoneyManagement from "../../Assets/Navbar/MoneyManagement.png";
+import ChevronLeftSmall from "../../Assets/Navbar/ChevronLeftSmall.png";
 
 const Sidebar = () => {
   return (
-    <ul className="space-y-2 px-[22px] py-2 text-white">
-      <li>
-        {/* <Link
-                          style={
-                            location.pathname === "/dashboard"
-                              ? {
-                                  background:
-                                    "linear-gradient(270deg, rgba(0, 0, 0, 0.45) 0%, rgba(0, 0, 0, 0.274309) 35.55%, rgba(0, 0, 0, 0) 100%), #6278FF",
-                                }
-                              : {}
-                          }
-                          to="/dashboard"
-                          className={`text-white font-normal rounded-[15px] flex items-center px-[20px] py-[13px]  group`}
-                        >
-                          {location.pathname === "/dashboard" ? (
-                            <img
-                              className=""
-                              // src={DashboardIconLight}
-                              alt="icon"
-                            />
-                          ) : (
-                            <img
-                              className=""
-                              // src={DashboardIconDark}
-                              alt="icon"
-                            />
-                          )}
+    <div className=" flex flex-col justify-between">
+      <ul className="space-y-2 px-[22px] py-2 ">
 
-                          <span
-                          className={`${
-                            location.pathname === "/dashboard"
-                              ? "text-white"
-                              : "text-[#8F8F8F]"
-                          } ml-3 text-[18px] font-[500]`}
-                          >
-                            Dashboard
-                          </span>
-                        </Link> */}
-      </li>
-    </ul>
+        <li>
+
+          <p className="flex items-center gap-2 text-[#34908E] text-[15px] font-semibold p-2"
+            style={{
+              borderRadius: "12px",
+              background: "rgba(52, 144, 142, 0.11)"
+
+            }}
+          ><img src={home} alt="icon" /> Home</p>
+
+        </li>
+        <li>
+
+          <p className="flex items-center gap-2  text-[15px] font-medium p-2"
+
+          ><img src={WatchingList} alt="icon" /> Watchlist</p>
+
+        </li>
+        <li>
+
+          <p className="flex items-center gap-2  text-[15px] font-medium p-2"
+
+          ><img src={Wallet} alt="icon" /> Wallet</p>
+
+        </li>
+        <li>
+
+          <p className="flex items-center gap-2  text-[15px] font-medium p-2"
+
+          ><img src={Search} alt="icon" /> Search</p>
+
+        </li>
+
+        <li>
+
+          <p className="flex items-center gap-2 text-[#fff] text-[15px] font-semibold p-2"
+            style={{
+              borderRadius: "12px",
+              background: "var(--brand-green, #34908E)"
+
+            }}
+          ><img src={MoneyManagement} alt="icon" /> Money Management</p>
+
+        </li>
+        <li>
+
+          <p className="flex items-center gap-2 text-[] text-[15px] font-semibold p-2"
+            style={{
+              borderRadius: "12px",
+              background: "var(--brand-lime-green, #D9F867)"
+
+            }}
+          ><img className="w-[20px]" alt="" /> Deposite</p>
+
+        </li>
+
+      </ul>
+
+      <div className="mt-[288px] ">
+        <ul className="space-y-2 px-[22px] py-2 ">
+
+          <li>
+
+            <p className="flex items-center gap-2 text-[#000000] text-[15px] font-semibold p-2 "
+              style={{
+                borderRadius: "12px",
+                background: "var(--system-background-light-secondary, #F2F2F7)"
+
+              }}
+            ><span className="w-[20px] h-[10px] "></span>  Language <span className="ms-4 text-[#3C3C4399]">العربية</span> <img src={ChevronLeftSmall} alt="icon"/></p>
+              
+
+          </li>
+          <li>
+
+            <p className=" text-center text-[#3C3C4399] text-[15px] font-semibold p-2"
+              style={{
+                borderRadius: "12px",
+                background: "var(--system-background-light-secondary, #F2F2F7)"
+
+              }}
+            > Language </p>
+              
+
+          </li>
+       
+        </ul>
+      </div>
+
+
+    </div>
+
   );
 };
 
