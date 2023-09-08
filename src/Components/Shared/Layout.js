2858aa511ae07d1f8ed391160d4d772098294fd3
Shajibul Alam Shihab <shihab77023@gmail.com>
2023-09-08 19:10:22 +0600

    update

--- src/Components/Shared/Layout.js
+++ src/Components/Shared/Layout.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-// import { Link, useLocation } from "react-router-dom";
 import Logo from "../../Assets/Shared/Logo.png";
+import Close from "../../Assets/Shared/Close.png";
 import Menu from "../../Assets/Shared/Menu.png";
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
@@ -36,13 +36,15 @@ const Layout = ({ children }) => {
       <div>
         <div>
           <nav
-            className={`bg-white fixed z-30 w-full lg:hidden ${toggleButton ? "" : "hidden"
-              }`}
+            className={`bg-white fixed z-30 w-full lg:hidden ${
+              toggleButton ? "" : "hidden"
+            }`}
           >
             <div className="px-3 py-3 lg:px-5 lg:pl-3">
               <div
-                className={`flex items-center justify-between ${toggleButton ? "" : "hidden"
-                  }`}
+                className={`flex items-center justify-between ${
+                  toggleButton ? "" : "hidden"
+                }`}
               >
                 <div className="flex items-center">
                   <button
@@ -57,18 +59,20 @@ const Layout = ({ children }) => {
                   <p>Menu</p>
                 </div>
 
-
                 <div className="flex items-center">
                   <div className="flex">
                     <NotificationsIcon />
-                    <p className="h-[20px] w-[20px] flex justify-center items-center text-xs font-medium ms-[-10px] mt-[-8px]"
+                    <p
+                      className="h-[20px] w-[20px] flex justify-center items-center text-xs font-medium ms-[-10px] mt-[-8px]"
                       style={{
                         borderRadius: "32px",
-                        border: "2px solid var(--system-background-light-primary, #FFF)",
-                        background: "var(--brand-lime-green, #D9F867)"
-
+                        border:
+                          "2px solid var(--system-background-light-primary, #FFF)",
+                        background: "var(--brand-lime-green, #D9F867)",
                       }}
-                    >3</p>
+                    >
+                      3
+                    </p>
                   </div>
                   <div className="bg-[#EAF3E6] rounded-[100px] h-[44px] w-[44px] flex justify-center items-center ms-3">
                     <img src={profile} alt="profile" />
@@ -80,35 +84,36 @@ const Layout = ({ children }) => {
           <div className="flex overflow-hidden">
             <aside
               id="sidebar"
-              className={`h-full min-h-[100vh] relative ${toggleButton ? "hidden" : ""
-                } z-20 top-0 bg-[#fff] border-r-[1px] left-0 flex lg:flex flex-shrink-0 flex-col w-[250px] transition duration-500 ease-in-out delay-150`}
+              className={` absolute md:relative h-full min-h-[100vh] z-[70] ${
+                toggleButton ? "hidden" : ""
+              } z-20 top-0 bg-[#fff] border-r-[1px] left-0 flex lg:flex flex-shrink-0 flex-col w-[250px] transition duration-500 ease-in-out delay-150 `}
               aria-label="Sidebar"
             >
               <div className="fixed bg-white h-full w-[250px] flex-1 flex flex-col min-h-0 pt-0">
                 <div className="flex-1 flex flex-col pb-4 overflow-y-auto">
                   <div className="flex-1 space-y-1">
                     <div className="py-8 h-[70px] flex items-center justify-between lg:justify-center">
-                      {/* <Link className="hidden lg:block" to={"/"}> */}
-                      {/* <img className="h-6 lg:h-8 " src={Logo} alt="icon" /> */}
-                      {/* </Link> */}
-                      {/* <p className="text-[#676767] ml-[27px] lg:hidden">Menu</p> */}
                       <div className="lg:flex items-center gap-[18px] hidden">
                         <p
                           style={{
                             borderRadius: "100px",
-                            background: "var(--brand-lime-green, #D9F867)"
+                            background: "var(--brand-lime-green, #D9F867)",
                           }}
-                        ><img src={logo} alt="icon" /></p>
-                        <p className=" text-[#000000] text-xl font-semibold">Tadawol</p>
+                        >
+                          <img src={logo} alt="icon" />
+                        </p>
+                        <p className=" text-[#000000] text-xl font-semibold">
+                          Tadawol
+                        </p>
                       </div>
                       <button
                         id="toggleSidebarMobile"
                         ariaExpanded="true"
                         ariaControls="sidebar"
-                        className="lg:hidden ms-2 mr-2 text-gray-600 hover:text-gray-900 cursor-pointer p-2 hover:bg-gray-100 focus:bg-gray-100 focus:ring-2 focus:ring-gray-100 rounded"
+                        className="lg:hidden ms-2 mr-2 text-gray-600 hover:text-gray-900 cursor-pointer p-2 hover:bg-gray-100 focus:bg-gray-100 focus:ring-2 focus:ring-gray-100 rounded flex items-center gap-2"
                         onClick={handleClick}
                       >
-                        {/* <img src={ArrowLeftIcon} alt="icon" /> */}X
+                        <img src={Close} alt="icon" />
                         <span className="ms-1"> Close</span>
                       </button>
                     </div>
@@ -117,7 +122,9 @@ const Layout = ({ children }) => {
                         <img src={profile} alt="profile" />
                       </div>
                       <div className="">
-                        <h1 className="text-[17px] font-semibold">Mohammed Al-Haj</h1>
+                        <h1 className="text-[17px] font-semibold">
+                          Mohammed Al-Haj
+                        </h1>
                         <p
                           className="flex text-[15px] font-normal"
                           style={{
@@ -134,7 +141,6 @@ const Layout = ({ children }) => {
                       <p>
                         <img src={ChevronLeftSmall} alt="icon" />
                       </p>
-
                     </div>
                     <Sidebar />
                   </div>
@@ -147,7 +153,7 @@ const Layout = ({ children }) => {
             ></div>
             <div id="main-content" className="h-full w-full relative ">
               <Navbar />
-              <main className="min-h-[100vh]">
+              <main className="min-h-[90vh]">
                 <div className="">{children}</div>
               </main>
             </div>
